a0ed7a4eeaed6e33b99a08dfec1515e6e3061758
Gabriel Pereira <gabrielpereiram10@gmail.com>
2022-09-25 17:44:54 -0300

    feat: adicionar projeto backend

---
+++ backend/src/test/java/com/gabriel/reservaareacomum/ReservaAreaComumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.reservaareacomum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaAreaComumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
